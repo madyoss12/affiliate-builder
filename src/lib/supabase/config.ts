36bdb132bc9c54c5a7f9a7f08bc2459a2a097862
madyoss12 <agencehijra@gmail.com>
2024-12-29 16:22:12 +0100

    fix: Use environment variables for Supabase config

--- src/lib/supabase/config.ts
+++ src/lib/supabase/config.ts
@@ -1,2 +1,5 @@
-export const SUPABASE_URL = 'https://xlzsnjxnkhjxngcxnpmx.supabase.co'
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsenNuanhua2hqeG5nY3hucG14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0MTc1NTQsImV4cCI6MjA1MDk5MzU1NH0.eJjBsHxPczPsreaDeplMqRKzRPre7CAG5Z8_7b6Hweo'
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+if (!SUPABASE_URL) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+if (!SUPABASE_ANON_KEY) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
